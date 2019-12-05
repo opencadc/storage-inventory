5eaa8fb123a96a44ecf72773617926e4f6293d72
Adrian Damian <Adrian.Damian@nrc.ca>
2019-12-05 15:40:03 -0800

    Finished intTest

---
+++ luskan/README.md
@@ -0,0 +1,87 @@
+# Storage Inventory TAP service
+
+This service allows queries to the metadata of the Storage Inventory.
+
+## Expected deployment
+This service instance is expected to have:
+- a proxy (HAproxy, apache, nginx) in front that performs SSL termination and forwards calls via HTTP on port 8080. Optional client certificates are passed through 
+via the X-Client-Certificate HTTP header (http-request set-header X-Client-Certificate %[ssl_c_der,base64]
+in haproxcy.cfg).
+- a database backend to store the TAP metadata
+- a reg service to resolve resource ids
+
+
+The container expects that a directory is attached to /conf and containing the following:
+
+## catalina.properties
+System properties required by tomcat:
+
+tomcat.connector.scheme=https
+
+tomcat.connector.proxyName={SSL terminator host name}
+
+tomcat.connector.proxyPort=443
+
+### database connection pool configuration
+luskan.tapadm.maxActive={}
+luskan.tapadm.username={}
+luskan.tapadm.password={}
+luskan.tapadm.url=jdbc:postgresql://postgres/content
+luskan.tapuser.maxActive={}
+luskan.tapuser.username={}
+luskan.tapuser.password={}
+luskan.tapuser.url=jdbc:postgresql://postgres/content
+luskan.uws.maxActive={}
+luskan.uws.username={}
+luskan.uws.password={}
+luskan.uws.url=jdbc:postgresql://postgres/content
+
+### optional intTest configuration
+ca.nrc.cadc.reg.client.RegistryClient.host={SSL terminator host name}
+ca.nrc.cadc.auth.BasicX509TrustManager.trust=true
+
+## LocalAuthority.properties
+
+### local authority map
+###
+### <base standardID> = <authority>
+ivo://ivoa.net/std/GMS#groups-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/GMS#search-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/UMS#users-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/UMS#reqs-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/UMS#login-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/UMS#modpass-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/UMS#resetpass-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/UMS#whoami-0.1 = ivo://cadc.nrc.ca/gms           
+ivo://ivoa.net/std/CDP#delegate-1.0 = ivo://cadc.nrc.ca/cred
+ivo://ivoa.net/std/CDP#proxy-1.0 = ivo://cadc.nrc.ca/cred
+
+
+## cadcproxy.pem 
+This optional certificate is used to use to make some priviledged server-to-server calls (A&A support).
+
+## cacerts
+This optional directory includes CA certificates (pem format) are added to the system trust store.
+
+## building it
+docker build -t luskan -f Dockerfile .
+
+## running it
+docker run -d --volume=/path/to/external/conf:/conf:ro --volume=/path/to/external/logs:/logs:rw --link postgres --name luskan luskan
+
+## checking it (while running)
+docker exec -it luskan
+
+## intTest
+In a container environment, the intTest could run as follows:
+1. Start postgres container (https://github.com/opencadc/docker-base/tree/master/cadc-postgresql-dev):
+    docker run --rm -d --volume=/path/to/external/:/logs:rw --name postgres cadc-postgresql-dev:latest
+
+2. Start haproxy container (https://github.com/opencadc/docker-base/tree/master/cadc-haproxy-dev)
+    docker run --rm -d -v /path/to/external/:/logs:rw -v /path/to/external/:/conf:ro -p 443:443 --link luskan:cadc-service --link reg:reg-service --name haproxy cadc-haproxy-dev:latest
+
+3. Start reg service using (https://github.com/opencadc/docker-base/tree/master/cadc-tomcat)
+4. Start luskan as above
+5. Link or copy a proxy certificate signed by a trusted CA or one of the CAs specified in /conf/cacerts to src/intTest/resource/testCert.pem
+6. Run command
+gradle -Dca.nrc.cadc.reg.client.RegistryClient.host={SSL terminator host name}  -Dca.nrc.cadc.auth.BasicX509TrustManager.trust=True intTest
---
+++ luskan/src/main/java/org/opencadc/luskan/ws/StorageTapService.java
@@ -0,0 +1,224 @@
+/*
+************************************************************************
+*******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+**************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+*
+*  (c) 2019.                            (c) 2019.
+*  Government of Canada                 Gouvernement du Canada
+*  National Research Council            Conseil national de recherches
+*  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+*  All rights reserved                  Tous droits réservés
+*
+*  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+*  expressed, implied, or               énoncée, implicite ou légale,
+*  statutory, of any kind with          de quelque nature que ce
+*  respect to the software,             soit, concernant le logiciel,
+*  including without limitation         y compris sans restriction
+*  any warranty of merchantability      toute garantie de valeur
+*  or fitness for a particular          marchande ou de pertinence
+*  purpose. NRC shall not be            pour un usage particulier.
+*  liable in any event for any          Le CNRC ne pourra en aucun cas
+*  damages, whether direct or           être tenu responsable de tout
+*  indirect, special or general,        dommage, direct ou indirect,
+*  consequential or incidental,         particulier ou général,
+*  arising from the use of the          accessoire ou fortuit, résultant
+*  software.  Neither the name          de l'utilisation du logiciel. Ni
+*  of the National Research             le nom du Conseil National de
+*  Council of Canada nor the            Recherches du Canada ni les noms
+*  names of its contributors may        de ses  participants ne peuvent
+*  be used to endorse or promote        être utilisés pour approuver ou
+*  products derived from this           promouvoir les produits dérivés
+*  software without specific prior      de ce logiciel sans autorisation
+*  written permission.                  préalable et particulière
+*                                       par écrit.
+*
+*  This file is part of the             Ce fichier fait partie du projet
+*  OpenCADC project.                    OpenCADC.
+*
+*  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+*  you can redistribute it and/or       vous pouvez le redistribuer ou le
+*  modify it under the terms of         modifier suivant les termes de
+*  the GNU Affero General Public        la “GNU Affero General Public
+*  License as published by the          License” telle que publiée
+*  Free Software Foundation,            par la Free Software Foundation
+*  either version 3 of the              : soit la version 3 de cette
+*  License, or (at your option)         licence, soit (à votre gré)
+*  any later version.                   toute version ultérieure.
+*
+*  OpenCADC is distributed in the       OpenCADC est distribué
+*  hope that it will be useful,         dans l’espoir qu’il vous
+*  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+*  without even the implied             GARANTIE : sans même la garantie
+*  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+*  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+*  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+*  General Public License for           Générale Publique GNU Affero
+*  more details.                        pour plus de détails.
+*
+*  You should have received             Vous devriez avoir reçu une
+*  a copy of the GNU Affero             copie de la Licence Générale
+*  General Public License along         Publique GNU Affero avec
+*  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+*  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+*                                       <http://www.gnu.org/licenses/>.
+*
+************************************************************************
+ */
+
+package org.opencadc.luskan.ws;
+
+import ca.nrc.cadc.auth.AuthenticationUtil;
+import ca.nrc.cadc.db.DBUtil;
+import org.opencadc.luskan.impl.DataSourceProviderImpl;
+import org.opencadc.luskan.impl.InitLuskanSchemaContent;
+import ca.nrc.cadc.reg.client.LocalAuthority;
+import ca.nrc.cadc.reg.client.RegistryClient;
+import ca.nrc.cadc.rest.RestAction;
+import ca.nrc.cadc.tap.schema.InitDatabaseTS;
+import ca.nrc.cadc.uws.server.impl.InitDatabaseUWS;
+import ca.nrc.cadc.vosi.AvailabilityPlugin;
+import ca.nrc.cadc.vosi.AvailabilityStatus;
+import ca.nrc.cadc.vosi.avail.CheckDataSource;
+import ca.nrc.cadc.vosi.avail.CheckException;
+import ca.nrc.cadc.vosi.avail.CheckResource;
+
+import java.security.AccessControlContext;
+import java.security.AccessController;
+import java.security.Principal;
+import javax.security.auth.Subject;
+import javax.security.auth.x500.X500Principal;
+import javax.sql.DataSource;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author adriand
+ */
+public class StorageTapService implements AvailabilityPlugin {
+
+    private static final Logger log = Logger.getLogger(StorageTapService.class);
+    
+    private static final Principal TRUSTED = new X500Principal("cn=servops_4a2,ou=cadc,o=hia,c=ca");
+
+    private static final String TAP_TEST = "select schema_name from tap_schema.schemas11 where schema_name='tap_schema'";
+    private static final String UWS_TEST = "select jobID from uws.Job limit 1";
+
+    private String appName;
+
+    public StorageTapService() {
+    }
+
+    @Override
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+    
+    @Override
+    public AvailabilityStatus getStatus() {
+        boolean isGood = true;
+        String note = "service is accepting queries";
+        try {
+            String state = getState();
+            if (RestAction.STATE_OFFLINE.equals(state)) {
+                return new AvailabilityStatus(false, null, null, null, RestAction.STATE_OFFLINE_MSG);
+            }
+            if (RestAction.STATE_READ_ONLY.equals(state)) {
+                return new AvailabilityStatus(false, null, null, null, RestAction.STATE_READ_ONLY_MSG);
+            }
+
+            String tapDsName = DataSourceProviderImpl.getDataSourceName(null, "tapadm");
+            DataSource tapadm = DBUtil.findJNDIDataSource(tapDsName);
+            InitDatabaseTS tsi = new InitDatabaseTS(tapadm, null, "tap_schema");
+            tsi.doInit();
+
+            String uwsDsName = DataSourceProviderImpl.getDataSourceName(null, "uws");
+            DataSource uws = DBUtil.findJNDIDataSource(uwsDsName);
+            InitDatabaseUWS uwsi = new InitDatabaseUWS(uws, null, "uws");
+            uwsi.doInit();
+
+            // create the TAP schema
+            InitLuskanSchemaContent lsc = new InitLuskanSchemaContent(tapadm, null, "tap_schema");
+            lsc.doInit();
+
+            // run a couple of queries
+            CheckResource cr = new CheckDataSource(tapDsName, TAP_TEST);
+            cr.check();
+
+            cr = new CheckDataSource(uwsDsName, UWS_TEST);
+            cr.check();
+
+            LocalAuthority localAuthority = new LocalAuthority();
+            RegistryClient reg = new RegistryClient();
+
+            // TODO - These do not work for intTest unless the testing environment deploys these services.
+//            URI credURI = localAuthority.getServiceURI(Standards.CRED_PROXY_10.toString());
+//            String url = reg.getServiceURL(credURI, Standards.VOSI_AVAILABILITY, AuthMethod.ANON).toExternalForm();
+//            CheckResource checkResource = new CheckWebService(url);
+//            //checkResource.check();
+//
+//            URI usersURI = localAuthority.getServiceURI(Standards.UMS_USERS_01.toString());
+//            url = reg.getServiceURL(usersURI, Standards.VOSI_AVAILABILITY, AuthMethod.ANON).toExternalForm();
+//            checkResource = new CheckWebService(url);
+//            //checkResource.check();
+//
+//            URI groupsURI = localAuthority.getServiceURI(Standards.GMS_SEARCH_01.toString());
+//            url = reg.getServiceURL(groupsURI, Standards.VOSI_AVAILABILITY, AuthMethod.ANON).toExternalForm();
+//            checkResource = new CheckWebService(url);
+//            checkResource.check();
+
+        } catch (CheckException ce) {
+            // tests determined that the resource is not working
+            isGood = false;
+            note = ce.getMessage();
+        } catch (Throwable t) {
+            // the test itself failed
+            log.error("test failed", t);
+            isGood = false;
+            note = "test failed, reason: " + t;
+        }
+        return new AvailabilityStatus(isGood, null, null, null, note);
+    }
+
+    @Override
+    public void setState(String state) {
+        AccessControlContext acContext = AccessController.getContext();
+        Subject subject = Subject.getSubject(acContext);
+
+        if (subject == null) {
+            return;
+        }
+
+        Principal caller = AuthenticationUtil.getX500Principal(subject);
+        if (AuthenticationUtil.equals(TRUSTED, caller)) {
+            String key = appName + RestAction.STATE_MODE_KEY;
+            if (RestAction.STATE_OFFLINE.equalsIgnoreCase(state)) {
+                System.setProperty(key, RestAction.STATE_OFFLINE);
+            //} else if (RestAction.STATE_READ_ONLY.equalsIgnoreCase(state)) {
+            //    System.setProperty(key, RestAction.STATE_READ_ONLY);
+            } else if (RestAction.STATE_READ_WRITE.equalsIgnoreCase(state)) {
+                System.setProperty(key, RestAction.STATE_READ_WRITE);
+            } else {
+                throw new IllegalArgumentException("invalid state: " + state 
+                    + " expected: " + RestAction.STATE_READ_WRITE + "|" + RestAction.STATE_OFFLINE);
+            }
+            log.info("WebService state changed: " + key + "=" + state + " by " + caller + " [OK]");
+        } else {
+            log.warn("WebService state change by " + caller + " [DENIED]");
+        }
+    }
+    
+    @Override
+    public boolean heartbeat() throws RuntimeException {
+        return true;
+    }
+
+    private String getState() {
+        String key = appName + RestAction.STATE_MODE_KEY;
+        String ret = System.getProperty(key);
+        if (ret == null) {
+            return RestAction.STATE_READ_WRITE;
+        }
+        return ret;
+    }
+
+}
