f215f153f1e8a051fe4e83c246ca326c71c557b6
Patrick Dowler <pdowler.cadc@gmail.com>
2019-12-09 16:40:47 -0800

    fix typo in create

--- cadc-inventory-db/src/main/resources/inventory.Artifact.sql
+++ cadc-inventory-db/src/main/resources/inventory.Artifact.sql
@@ -10,7 +10,7 @@ create table <schema>.Artifact (
 
     siteLocations uuid[],
     storageLocation_storageID varchar(256),
-    storageLocation_storageBucket varchar(8);
+    storageLocation_storageBucket varchar(8),
 
     lastModified timestamp not null,
     metaChecksum varchar(136) not null,
