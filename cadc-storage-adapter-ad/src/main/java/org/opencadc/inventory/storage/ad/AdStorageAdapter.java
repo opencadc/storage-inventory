9cc6f34594adcf28e8ae0168f01ecb2905589a54
Helena Jeeves <Helena.Jeeves@nrc-cnrc.gc.ca>
2020-02-06 16:03:17 -0800

    s2587, ta 12867: first checkin of new module. Stubs of adapter and tests in place. README file there, but little content for now.

---
+++ cadc-storage-adapter-ad/src/main/java/org/opencadc/inventory/storage/ad/AdStorageAdapter.java
@@ -0,0 +1,238 @@
+/*
+ ************************************************************************
+ *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
+ **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
+ *
+ *  (c) 2020.                            (c) 2020.
+ *  Government of Canada                 Gouvernement du Canada
+ *  National Research Council            Conseil national de recherches
+ *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
+ *  All rights reserved                  Tous droits réservés
+ *
+ *  NRC disclaims any warranties,        Le CNRC dénie toute garantie
+ *  expressed, implied, or               énoncée, implicite ou légale,
+ *  statutory, of any kind with          de quelque nature que ce
+ *  respect to the software,             soit, concernant le logiciel,
+ *  including without limitation         y compris sans restriction
+ *  any warranty of merchantability      toute garantie de valeur
+ *  or fitness for a particular          marchande ou de pertinence
+ *  purpose. NRC shall not be            pour un usage particulier.
+ *  liable in any event for any          Le CNRC ne pourra en aucun cas
+ *  damages, whether direct or           être tenu responsable de tout
+ *  indirect, special or general,        dommage, direct ou indirect,
+ *  consequential or incidental,         particulier ou général,
+ *  arising from the use of the          accessoire ou fortuit, résultant
+ *  software.  Neither the name          de l'utilisation du logiciel. Ni
+ *  of the National Research             le nom du Conseil National de
+ *  Council of Canada nor the            Recherches du Canada ni les noms
+ *  names of its contributors may        de ses  participants ne peuvent
+ *  be used to endorse or promote        être utilisés pour approuver ou
+ *  products derived from this           promouvoir les produits dérivés
+ *  software without specific prior      de ce logiciel sans autorisation
+ *  written permission.                  préalable et particulière
+ *                                       par écrit.
+ *
+ *  This file is part of the             Ce fichier fait partie du projet
+ *  OpenCADC project.                    OpenCADC.
+ *
+ *  OpenCADC is free software:           OpenCADC est un logiciel libre ;
+ *  you can redistribute it and/or       vous pouvez le redistribuer ou le
+ *  modify it under the terms of         modifier suivant les termes de
+ *  the GNU Affero General Public        la “GNU Affero General Public
+ *  License as published by the          License” telle que publiée
+ *  Free Software Foundation,            par la Free Software Foundation
+ *  either version 3 of the              : soit la version 3 de cette
+ *  License, or (at your option)         licence, soit (à votre gré)
+ *  any later version.                   toute version ultérieure.
+ *
+ *  OpenCADC is distributed in the       OpenCADC est distribué
+ *  hope that it will be useful,         dans l’espoir qu’il vous
+ *  but WITHOUT ANY WARRANTY;            sera utile, mais SANS AUCUNE
+ *  without even the implied             GARANTIE : sans même la garantie
+ *  warranty of MERCHANTABILITY          implicite de COMMERCIALISABILITÉ
+ *  or FITNESS FOR A PARTICULAR          ni d’ADÉQUATION À UN OBJECTIF
+ *  PURPOSE.  See the GNU Affero         PARTICULIER. Consultez la Licence
+ *  General Public License for           Générale Publique GNU Affero
+ *  more details.                        pour plus de détails.
+ *
+ *  You should have received             Vous devriez avoir reçu une
+ *  a copy of the GNU Affero             copie de la Licence Générale
+ *  General Public License along         Publique GNU Affero avec
+ *  with OpenCADC.  If not, see          OpenCADC ; si ce n’est
+ *  <http://www.gnu.org/licenses/>.      pas le cas, consultez :
+ *                                       <http://www.gnu.org/licenses/>.
+ *
+ *  $Revision: 4 $
+ *
+ ************************************************************************
+ */
+
+package org.opencadc.inventory.storage.ad;
+
+import ca.nrc.cadc.net.IncorrectContentChecksumException;
+import ca.nrc.cadc.net.IncorrectContentLengthException;
+import ca.nrc.cadc.net.ResourceNotFoundException;
+import ca.nrc.cadc.net.TransientException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.SortedSet;
+
+import org.apache.log4j.Logger;
+import org.opencadc.inventory.InventoryUtil;
+import org.opencadc.inventory.StorageLocation;
+import org.opencadc.inventory.storage.NewArtifact;
+import org.opencadc.inventory.storage.ReadException;
+import org.opencadc.inventory.storage.StorageAdapter;
+import org.opencadc.inventory.storage.StorageEngageException;
+import org.opencadc.inventory.storage.StorageMetadata;
+import org.opencadc.inventory.storage.ThreadedIO;
+import org.opencadc.inventory.storage.WriteException;
+
+/**
+ * The interface to storage implementations.
+ * 
+ * @author jeevesh
+ *
+ */
+public class AdStorageAdapter implements StorageAdapter {
+
+    private static final Logger log = Logger.getLogger(AdStorageAdapter.class);
+
+    /**
+     * Construct an AdStorageAdapter with the config stored in the
+     * well-known properties file with well-known properties.
+     */
+    public AdStorageAdapter(){}
+
+    /**
+     * Get from storage the artifact identified by storageLocation.
+     * 
+     * @param storageLocation The storage location containing storageID and storageBucket.
+     * @param dest The destination stream.
+     * 
+     * @throws ResourceNotFoundException If the artifact could not be found.
+     * @throws ReadException If the storage system failed to stream.
+     * @throws WriteException If the client failed to stream.
+     * @throws StorageEngageException If the adapter failed to interact with storage.
+     * @throws TransientException If an unexpected, temporary exception occurred. 
+     */
+    public void get(StorageLocation storageLocation, OutputStream dest)
+        throws ResourceNotFoundException, ReadException, WriteException, StorageEngageException, TransientException {
+        throw new UnsupportedOperationException("not supported");
+    }
+
+    
+    /**
+     * Get from storage the artifact identified by storageLocation.
+     * 
+     * @param storageLocation The storage location containing storageID and storageBucket.
+     * @param dest The destination stream.
+     * @param operations operations to be applied to the artifact
+     * 
+     * @throws ResourceNotFoundException If the artifact could not be found.
+     * @throws ReadException If the storage system failed to stream.
+     * @throws WriteException If the client failed to stream.
+     * @throws StorageEngageException If the adapter failed to interact with storage.
+     * @throws TransientException If an unexpected, temporary exception occurred. 
+     */
+    public void get(StorageLocation storageLocation, OutputStream dest, Set<String> operations)
+        throws ResourceNotFoundException, ReadException, WriteException, StorageEngageException, TransientException {
+        throw new UnsupportedOperationException("not supported");
+    }
+    
+    /**
+     * Write an artifact to storage. The returned storage location will be used for future get and 
+     * delete calls. If the storage implementation overwrites a previously used StorageLocation, it must
+     * perform an atomic replace and leave the previously stored bytes in tact if the put fails. The storage
+     * implementation may always write to a new storage location (e.g. generated unique storageID); in this case,
+     * the caller is responsible for keeping track of and cleaning up previously stored objects (the previous 
+     * StorageLocation of an Artifact). 
+     * The value of storageBucket in the returned StorageMetadata and StorageLocation can be used to
+     * retrieve batches of artifacts in some of the iterator signatures defined in this interface.
+     * Batches of artifacts can be listed by bucket in two of the iterator methods in this interface.
+     * If storageBucket is null then the caller will not be able perform bucket-based batch
+     * validation through the iterator methods.
+     * 
+     * @param newArtifact The holds information about the incoming artifact.  If the contentChecksum
+     *     and contentLength are set, they will be used to validate the bytes received.
+     * @param source The stream from which to read.
+     * @return The storage metadata.
+     * 
+     * @throws IncorrectContentChecksumException If the calculated checksum does not the expected
+     *     checksum as described in newArtifact.
+     * @throws IncorrectContentLengthException If the calculated length does not the expected
+     *     length as described in newArtifact.
+     * @throws ReadException If the client failed to stream.
+     * @throws WriteException If the storage system failed to stream.
+     * @throws StorageEngageException If the adapter failed to interact with storage.
+     * @throws TransientException If an unexpected, temporary exception occurred.
+     */
+    public StorageMetadata put(NewArtifact newArtifact, InputStream source)
+        throws IncorrectContentChecksumException, IncorrectContentLengthException, ReadException,
+            WriteException, StorageEngageException, TransientException {
+        throw new UnsupportedOperationException("not supported");
+    }
+        
+    /**
+     * Delete from storage the artifact identified by storageLocation.
+     * @param storageLocation Identifies the artifact to delete.
+     * 
+     * @throws ResourceNotFoundException If the artifact could not be found.
+     * @throws IOException If an unrecoverable error occurred.
+     * @throws StorageEngageException If the adapter failed to interact with storage.
+     * @throws TransientException If an unexpected, temporary exception occurred. 
+     */
+    public void delete(StorageLocation storageLocation)
+        throws ResourceNotFoundException, IOException, StorageEngageException, TransientException {
+        throw new UnsupportedOperationException("not supported");
+    }
+    
+    /**
+     * Iterator of items ordered by their storageIDs.
+     * @return An iterator over an ordered list of items in storage.
+     * 
+     * @throws StorageEngageException If the adapter failed to interact with storage.
+     * @throws TransientException If an unexpected, temporary exception occurred. 
+     */
+    public Iterator<StorageMetadata> iterator()
+        throws StorageEngageException, TransientException {
+        throw new UnsupportedOperationException("not supported");
+    }
+    
+    /**
+     * Iterator of items ordered by their storageIDs in the given bucket.
+     * @param storageBucket Only iterate over items in this bucket.
+     * @return An iterator over an ordered list of items in this storage bucket.
+     * 
+     * @throws StorageEngageException If the adapter failed to interact with storage.
+     * @throws TransientException If an unexpected, temporary exception occurred. 
+     */
+    public Iterator<StorageMetadata> iterator(String storageBucket)
+        throws StorageEngageException, TransientException {
+        throw new UnsupportedOperationException("not supported");
+    }
+    /**
+     * Get a set of  of items in the given bucket.
+     * @param storageBucket the bucket to list
+     * @return An iterator over an ordered list of items in this storage bucket.
+     * 
+     * @throws StorageEngageException If the adapter failed to interact with storage.
+     * @throws TransientException If an unexpected, temporary exception occurred. 
+     */
+    public SortedSet<StorageMetadata> list(String storageBucket)
+        throws StorageEngageException, TransientException {
+        throw new UnsupportedOperationException("not supported");
+    }
+    
+    // for a symbolic bucket scheme (eg AD or human-usable filesystem):
+    //public String[] getStorageBucketDelimiters();
+    // public int getMaxDepth();
+    //public SortedSet<String> getBuckets(int len);
+    
+    // for a programmatic prefixable bucket scheme:
+    // char set specified as lower case hex [0-9a-f]
+    //public int getMaxBucketLength();
+}
