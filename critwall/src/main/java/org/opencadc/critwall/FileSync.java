1ce1bbc5bfc745561f70d0927522e4e749b7d1e3
Helena Jeeves <Helena.Jeeves@nrc-cnrc.gc.ca>
2020-04-17 13:57:15 -0700

    s2575: ta13128: FileSync: remove commented code

--- critwall/src/main/java/org/opencadc/critwall/FileSync.java
+++ critwall/src/main/java/org/opencadc/critwall/FileSync.java
@@ -124,9 +124,10 @@ public class FileSync {
         this.selector = selector;
         this.nthreads = nthreads;
 
-        throw new UnsupportedOperationException("TODO");
-
         // To be completed in s2575, ta 13061
+        // TODO: temporary so that FileSync.run can execute remove setting
+        // locator to null when this section is finished
+        this.locator = null;
         //        try {
         //            RegistryClient rc = new RegistryClient();
         //            Capabilities caps = rc.getCapabilities(resourceID);
