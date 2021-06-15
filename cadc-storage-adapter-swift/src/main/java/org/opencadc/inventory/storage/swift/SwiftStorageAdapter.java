dbf8cbdff0228063fd7151cd241452bdd6d3b706
Patrick Dowler <pdowler.cadc@gmail.com>
2021-06-15 16:42:10 -0700

    improved SwiftStorageAdapter.INIT

--- cadc-storage-adapter-swift/src/main/java/org/opencadc/inventory/storage/swift/SwiftStorageAdapter.java
+++ cadc-storage-adapter-swift/src/main/java/org/opencadc/inventory/storage/swift/SwiftStorageAdapter.java
@@ -284,20 +284,20 @@ public class SwiftStorageAdapter  implements StorageAdapter {
         try {
             long t1;
             
-            log.info("creating client...");
+            log.debug("creating client/authenticate...");
             t1 = System.currentTimeMillis();
             this.client = new AccountFactory(ac).createAccount();
             client.setAllowContainerCaching(true);
             client.setAllowReauthenticate(true);
             final long authTime = System.currentTimeMillis() - t1;
-            log.info("creating client: " + authTime);
+            log.debug("creating client/authenticate: " + authTime);
             
             // base-name bucket to store transient content and config attributes
-            log.info("get base storageBucket...");
+            log.debug("get base storageBucket...");
             t1 = System.currentTimeMillis();
             Container c = client.getContainer(storageBucket);
             final long bucketTime = System.currentTimeMillis() - t1;
-            log.info("get base storageBucket: " + bucketTime);
+            log.debug("get base storageBucket: " + bucketTime);
             
             log.info("SwiftStorageAdapter.INIT authTime=" + authTime + " bucketTime=" + bucketTime);
             init(c);
@@ -311,13 +311,11 @@ public class SwiftStorageAdapter  implements StorageAdapter {
 
     
     private void init(Container c) throws InvalidConfigException, StorageEngageException {
-        
         if (!c.exists()) {
             log.debug("creating: " + c.getName());
             c.create();
             log.debug("created: " + c.getName());
         }
-        
         
         // check vs config
         Map<String,Object> curmeta = c.getMetadata();
