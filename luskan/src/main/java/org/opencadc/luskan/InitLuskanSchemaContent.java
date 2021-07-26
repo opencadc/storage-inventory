c30c84be7d711b28f4855f55bb2c57482e93f076
Patrick Dowler <pdowler.cadc@gmail.com>
2021-07-26 10:44:52 -0700

    added infiltered inventory.ArtifactMetadata view to help diagnose storage site content

--- luskan/src/main/java/org/opencadc/luskan/InitLuskanSchemaContent.java
+++ luskan/src/main/java/org/opencadc/luskan/InitLuskanSchemaContent.java
@@ -86,8 +86,8 @@ import javax.sql.DataSource;
 public class InitLuskanSchemaContent extends InitDatabase {
 
     public static final String MODEL_NAME = "luskan-schema";
-    public static final String MODEL_VERSION = "0.5.2";
-    public static final String PREV_MODEL_VERSION = "0.5.1";
+    public static final String MODEL_VERSION = "0.5.3";
+    public static final String PREV_MODEL_VERSION = "0.5.2";
 
     // the SQL is tightly coupled to cadc-tap-schema table names (for TAP-1.1)
     static String[] CREATE_SQL = new String[] {
