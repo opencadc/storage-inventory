8bd50b22abddb29d503a2913fe73040112b40c04
Patrick Dowler <pdowler.cadc@gmail.com>
2022-05-12 16:04:07 -0700

    cadc-inventory-db: add StorageLocationEventDAO

--- cadc-inventory-db/src/main/resources/inventory.vacuum-analyze.sql
+++ cadc-inventory-db/src/main/resources/inventory.vacuum-analyze.sql
@@ -2,5 +2,6 @@
 VACUUM VERBOSE ANALYZE <schema>.Artifact;
 VACUUM VERBOSE ANALYZE <schema>.DeletedArtifact;
 VACUUM VERBOSE ANALYZE <schema>.DeletedStorageLocation;
-VACUUM VERBOSE ANALYZE <schema>.DeletedStorageLocation;
 VACUUM VERBOSE ANALYZE <schema>.ObsoleteStorageLocation;
+VACUUM VERBOSE ANALYZE <schema>.StorageLocation;
+VACUUM VERBOSE ANALYZE <schema>.HarvestState;
