2387d3aad1fda32890c32d3c9dcc48c6ccdf4218
Patrick Dowler <pdowler.cadc@gmail.com>
2024-03-11 19:10:14 -0700

    vault: ArtifactSync init intTest enable

--- vault/src/intTest/java/org/opencadc/vault/Constants.java
+++ vault/src/intTest/java/org/opencadc/vault/Constants.java
@@ -3,7 +3,7 @@
 *******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 **************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 *
-*  (c) 2023.                            (c) 2023.
+*  (c) 2024.                            (c) 2024.
 *  Government of Canada                 Gouvernement du Canada
 *  National Research Council            Conseil national de recherches
 *  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -67,91 +67,26 @@
 
 package org.opencadc.vault;
 
-import ca.nrc.cadc.db.ConnectionConfig;
-import ca.nrc.cadc.db.DBConfig;
-import ca.nrc.cadc.db.DBUtil;
 import ca.nrc.cadc.util.FileUtil;
-import ca.nrc.cadc.util.Log4jInit;
 import java.io.File;
 import java.net.URI;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.opencadc.inventory.StorageSite;
-import org.opencadc.inventory.db.SQLGenerator;
-import org.opencadc.inventory.db.StorageSiteDAO;
+import org.opencadc.gms.GroupURI;
 
 /**
  *
  * @author pdowler
  */
-public class TransferTest extends org.opencadc.conformance.vos.TransferTest {
-    private static final Logger log = Logger.getLogger(TransferTest.class);
+public class Constants {
+    private static final Logger log = Logger.getLogger(Constants.class);
 
-    static {
-        Log4jInit.setLevel("org.opencadc.vault", Level.INFO);
-        Log4jInit.setLevel("org.opencadc.conformance.vos", Level.INFO);
-        Log4jInit.setLevel("org.opencadc.vospace", Level.INFO);
-        Log4jInit.setLevel("ca.nrc.cadc.net", Level.INFO);
-    }
+    static URI RESOURCE_ID = URI.create("ivo://opencadc.org/vault");
     
-    // these are the same as raven intTest
-    static String SERVER = "VAULT_TEST";
-    static String DATABASE = "cadctest";
-    static String SCHEMA = "inventory";
+    static File ADMIN_CERT = FileUtil.getFileFromResource("vault-test.pem", Constants.class);
+    static File ALT_CERT = FileUtil.getFileFromResource("vault-auth-test.pem", Constants.class);
     
-    public TransferTest() {
-        super(Constants.RESOURCE_ID, Constants.ADMIN_CERT);
-        enableTestDataNodePermission(Constants.ALT_GROUP, Constants.ALT_CERT);
-    }
+    static GroupURI ALT_GROUP = new GroupURI(URI.create("ivo://cadc.nrc.ca/gms?opencadc-vospace-test"));
     
-    @Before
-    public void checkGlobal() throws Exception {
-        // make sure inventory.StorageSite has a readable/writable ivo://opencadc.org/minoc
-        
-        try {
-            DBConfig dbrc = new DBConfig();
-            ConnectionConfig cc = dbrc.getConnectionConfig(SERVER, DATABASE);
-            DBUtil.createJNDIDataSource("jdbc/inventory", cc);
-
-            Map<String,Object> config = new TreeMap<String,Object>();
-            config.put(SQLGenerator.class.getName(), SQLGenerator.class);
-            config.put("jndiDataSourceName", "jdbc/inventory");
-            config.put("invSchema", SCHEMA);
-            config.put("genSchema", SCHEMA);
-            
-            StorageSiteDAO dao = new StorageSiteDAO(false);
-            dao.setConfig(config);
-            Set<StorageSite> sites = dao.list();
-            if (sites.isEmpty()) {
-                StorageSite ss = new StorageSite(URI.create("ivo://opencadc.org/minoc"), "vault-test-minoc", true, true);
-                dao.put(ss);
-                log.info("created record in local db: " + ss);
-            }
-            sites = dao.list();
-            for (StorageSite ss : sites) {
-                log.info("storage sites in local db: " + ss);
-            }
-        } catch (Exception ex) {
-            log.error("setup failed", ex);
-            throw ex;
-        }
+    private Constants() { 
     }
-
-    @Override
-    public void asyncMoveTest() {
-        super.asyncMoveTest();
-    }
-
-    @Ignore
-    @Override
-    public void syncPushPullTest() {
-        super.syncPushPullTest();
-    }
-    
-    
 }
